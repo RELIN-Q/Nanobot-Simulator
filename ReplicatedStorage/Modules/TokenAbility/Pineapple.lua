bc522240633253c66613895da8c437131580737a
Jordan Mattmiller <jordanmattmiller@gmail.com>
2025-07-15 14:07:27 -0500

    Updated ReplicatedStorage/Modules/TokenAbility/Pineapple.lua

---
+++ ReplicatedStorage/Modules/TokenAbility/Pineapple.lua
@@ -0,0 +1,17 @@
+-- @ScriptType: ModuleScript
+local tokene = {}
+
+
+local rp = game:GetService("ReplicatedStorage")
+local notify = require(rp.Modules:WaitForChild("NotificationModule"))
+local Updater = require(rp.Modules:WaitForChild("Update"))
+function tokene.Collect(plr,pos,bee,token)
+	local pdata = _G.PlayerData[plr.Name]
+	local inv = pdata.Inventory
+	local stats = pdata.Stats
+	notify.Create(plr,{Message="+"..token.Amount.Value.." Pineapple",Type="Default"})
+	inv.Pineapple += token.Amount.Value
+	Updater.Inventory(plr)
+end
+
+return tokene
